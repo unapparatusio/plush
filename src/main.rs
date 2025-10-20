9bb5b80056ab17d85d5df71eb27b086db16842b8
Unapparatus IO <unapparatus.io@proton.me>
2025-10-20 12:05:30 +0530

    Command Execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::process;
+
+fn execute(args: &Vec<&str>)
+{
+    let (program, args) = args.split_at(1);
+    let program = &program[0];
+
+    process::Command::new(program)
+        .args(args)
+        .status();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    let args = line.split(" ").collect();
+
+    execute(&args);
 }
