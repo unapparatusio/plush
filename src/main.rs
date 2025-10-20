2b9a2cc1a75f18f6b98891177f57d369c2f1b7c3
Unapparatus IO <unapparatus.io@proton.me>
2025-10-20 12:57:19 +0530

    Fixed trailing newline interfering with execution of commands, and implemented the main loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self, Write};
 use std::process;
 
 fn execute(args: &Vec<&str>)
@@ -6,16 +6,31 @@ fn execute(args: &Vec<&str>)
     let (program, args) = args.split_at(1);
     let program = &program[0];
 
-    process::Command::new(program)
+    let _ = process::Command::new(program)
         .args(args)
-        .status();
+        .status()
+        .map_err(|e| eprintln!("plush: error while executing program: {e}"));
 }
 
 fn main() {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
+    loop
+    {
+        let mut line = String::new();
 
-    let args = line.split(" ").collect();
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
 
-    execute(&args);
+        if line.is_empty()
+        {
+            println!();
+            continue;
+        }
+        
+        let args = line.split(" ").collect();
+
+        execute(&args);
+        
+    }
 }
