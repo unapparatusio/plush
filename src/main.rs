a3de9ce56500f87f59240322fb336972c86be28d
Unapparatus IO <unapparatus.io@proton.me>
2025-10-20 13:58:49 +0530

    Implemented BUILTINS

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
 use std::io::{self, Write};
 use std::process;
 
-fn execute(args: &Vec<&str>)
-{
-    let (program, args) = args.split_at(1);
-    let program = &program[0];
+mod builtins {
+    use std::process;
+    use std::env;
+    
+    pub const BUILTINS: [(&str, fn(&[&str])); 2] = [
+        ("exit", exit),
+        ("cd", cd),
+    ];
+    
+    fn exit(args: &[&str]) {
+        if args.len() == 0 {
+            process::exit(0);
+        }
 
+        eprintln!("The command `exit` takes no arguments");
+    }
+
+    fn cd(args: &[&str]) {
+        if args.len() == 1 {
+            env::set_current_dir(args[0])
+                .map_err(|e| eprintln!("cd: {e}"));
+        } else {
+            eprintln!("cd: one argument needed, found: {}", args.len());
+        }
+    }
+}
+
+fn execute(args: &Vec<&str>) {
+    use crate::builtins::*;
+
+    let (root, args) = args.split_at(1);
+    let root = &root[0];
+
+    for (cmd, subr) in BUILTINS.iter() {
+        if root == cmd {
+            subr(args);
+            return;
+        }
+    }
+
+    launch(root, args);
+
+}
+
+fn launch(program: &str, args: &[&str]) {
     let _ = process::Command::new(program)
         .args(args)
         .status()
@@ -13,8 +53,7 @@ fn execute(args: &Vec<&str>)
 }
 
 fn main() {
-    loop
-    {
+    loop {
         let mut line = String::new();
 
         print!("> ");
@@ -22,8 +61,7 @@ fn main() {
         io::stdin().read_line(&mut line).unwrap();
         let line = line.trim();
 
-        if line.is_empty()
-        {
+        if line.is_empty() {
             println!();
             continue;
         }
