efd1bd813faa80e606221eedbfaa73ba5065b417
Unapparatus IO <unapparatus.io@proton.me>
2025-10-27 19:12:23 +0530

    refactor(builtins): The `mod builtins` has its own file

---
+++ src/builtins.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process;
+use crate::*;
+
+pub const BUILTINS: [(&str, fn(Args)); 2] = [
+    ("exit", exit),
+    ("cd", cd),
+];
+
+fn exit(args: Args) {
+    if args.len() == 0 {
+        process::exit(0);
+    }
+
+    eprintln!("The command `exit` takes no arguments");
+}
+
+fn cd(args: Args) {
+    if args.len() == 1 {
+        let _ = env::set_current_dir(args.as_slice()[0])
+            .map_err(|e| eprintln!("cd: {e}"));
+    } else {
+        eprintln!("cd: one argument needed, found: {}", args.len());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod builtins;
+
 use std::process;
 use std::path::{PathBuf, Path};
 use std::env;
@@ -49,34 +51,6 @@ impl<'a, T> From<&'a T> for Args<'a>
         Args
         {
             inner
-        }
-    }
-}
-
-mod builtins {
-    use std::env;
-    use std::process;
-    use crate::*;
-    
-    pub const BUILTINS: [(&str, fn(Args)); 2] = [
-        ("exit", exit),
-        ("cd", cd),
-    ];
-    
-    fn exit(args: Args) {
-        if args.len() == 0 {
-            process::exit(0);
-        }
-
-        eprintln!("The command `exit` takes no arguments");
-    }
-
-    fn cd(args: Args) {
-        if args.len() == 1 {
-            let _ = env::set_current_dir(args.as_slice()[0])
-                .map_err(|e| eprintln!("cd: {e}"));
-        } else {
-            eprintln!("cd: one argument needed, found: {}", args.len());
         }
     }
 }
